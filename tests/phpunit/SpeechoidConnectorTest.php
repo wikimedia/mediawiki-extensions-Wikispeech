a8927a3b78c45fd6e01011854e04c42778972e4a
kalle <karl.wettin@wikimedia.se>
2020-09-15 18:55:12 +0200

    Request default voice per language from Speechoid
    Introduce use of REST endpoint introduced in Wikispeech-server
    that respons with default voice per language, rather than using
    the old hard coded empty array.
    
    Requires Speechoid >= 0.1.2.
    
    Bug: T262948
    Change-Id: I7f2b7ce9cdcfeea9e3712ff2948d90f86937139b

---
+++ tests/phpunit/SpeechoidConnectorTest.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * @file
+ * @ingroup Extensions
+ * @license GPL-2.0-or-later
+ */
+
+/**
+ * @group medium
+ * @covers SpeechoidConnector
+ */
+class SpeechoidConnectorTest extends MediaWikiIntegrationTestCase {
+
+	public function testListDefaultVoicePerLanguage_mockedSpeechoidResponse_parsedCorrectly() {
+		$defaultVoicesJson = '[' .
+			'{"default_textprocessor": "marytts_textproc_sv",' .
+			' "default_voice": "stts_sv_nst-hsmm", "lang": "sv"}, ' .
+			'{"default_textprocessor": "marytts_textproc_nb",' .
+			' "default_voice": "stts_no_nst-hsmm", "lang": "nb"}, ' .
+			'{"default_textprocessor": "marytts_textproc_en",' .
+			' "default_voice": "cmu-slt-hsmm", "lang": "en"}, ' .
+			'{"default_textprocessor": "marytts_textproc_ar",' .
+			' "default_voice": "ar-nah-hsmm", "lang": "ar"}' .
+			']';
+		$connectorMock = $this->createPartialMock(
+			SpeechoidConnector::class,
+			[ 'requestDefaultVoices' ]
+		);
+		$connectorMock
+			->expects( $this->once() )
+			->method( 'requestDefaultVoices' )
+			->willReturn( $defaultVoicesJson );
+
+		$defaultVoicePerLanguage = $connectorMock->listDefaultVoicePerLanguage();
+
+		$this->assertCount( 4, $defaultVoicePerLanguage );
+		$this->assertEquals( 'stts_sv_nst-hsmm', $defaultVoicePerLanguage['sv'] );
+		$this->assertEquals( 'stts_no_nst-hsmm', $defaultVoicePerLanguage['nb'] );
+		$this->assertEquals( 'cmu-slt-hsmm', $defaultVoicePerLanguage['en'] );
+		$this->assertEquals( 'ar-nah-hsmm', $defaultVoicePerLanguage['ar'] );
+	}
+
+}

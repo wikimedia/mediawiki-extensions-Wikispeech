3fc8b6eb9645e512c3397d8c2a07b0b864bd80e2
Sebastian Berlin <sebastian.berlin@wikimedia.se>
2021-02-24 09:35:15 +0100

    Add color flag to Phan script
    Change-Id: I80b82436a7812cbe8ecc103bb15fa7a91a8d4a8c

--- dev/phan.sh
+++ dev/phan.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
 
-/vagrant/mediawiki/extensions/Wikispeech/vendor/bin/phan -d /vagrant/mediawiki/extensions/Wikispeech/ -p
+/vagrant/mediawiki/extensions/Wikispeech/vendor/bin/phan -d /vagrant/mediawiki/extensions/Wikispeech/ -p --color

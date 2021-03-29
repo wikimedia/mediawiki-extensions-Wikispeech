bc476643498a59a9010b90996e81361d31257b3a
kalle <karl.wettin@wikimedia.se>
2021-03-29 12:50:40 +0200

    Lexicon entry delete tests
    Simulates various successful and failing text responses from Pronlex.
    
    Bug: T278687
    Change-Id: I12e6bc4350fdcfcc12fa27a4f8591dc26fe16e5d

--- tests/phpunit/unit/SpeechoidConnectorTest.php
+++ tests/phpunit/unit/SpeechoidConnectorTest.php
@@ -12,11 +12,22 @@ use InvalidArgumentException;
 use MediaWiki\Http\HttpRequestFactory;
 use MediaWiki\Wikispeech\SpeechoidConnector;
 use MediaWikiUnitTestCase;
+use PHPUnit\Framework\MockObject\MockObject;
 
 /**
  * @covers \MediaWiki\Wikispeech\SpeechoidConnector
  */
 class SpeechoidConnectorTest extends MediaWikiUnitTestCase {
+
+	/** @var HashConfig */
+	private $config;
+
+	/** @var HttpRequestFactory|MockObject */
+	private $requestFactory;
+
+	/** @var SpeechoidConnector|MockObject */
+	private $speechoidConnector;
+
 	protected function setUp() : void {
 		$this->requestFactory = $this->createMock( HttpRequestFactory::class );
 		$this->config = new HashConfig();
@@ -113,5 +124,68 @@ class SpeechoidConnectorTest extends MediaWikiUnitTestCase {
 		);
 
 		$this->assertSame( 'sampa transcription', $sampa );
+	}
+
+	public function testDeleteLexiconEntry_goodLexiconNameAndIdentityGiven_isOK() {
+		$this->config->set( 'WikispeechSpeechoidUrl', 'speechoid.url' );
+		$this->requestFactory
+			->method( 'get' )
+			->with( 'speechoid.url/lexserver/lexicon/delete_entry/lexiconName/identity' )
+			->willReturn(
+				"deleted entry id 'identity' from lexicon 'lexiconName'"
+			);
+		$status = $this->speechoidConnector->deleteLexiconEntry(
+			'lexiconName',
+			'identity'
+		);
+		$this->assertTrue( $status->isOK() );
+	}
+
+	public function testDeleteLexiconEntry_badLexiconNameGiven_isNotOK() {
+		$this->config->set( 'WikispeechSpeechoidUrl', 'speechoid.url' );
+		$this->requestFactory
+			->method( 'get' )
+			->with( 'speechoid.url/lexserver/lexicon/delete_entry/lexiconName/identity' )
+			->willReturn(
+			// phpcs:ignore Generic.Files.LineLength.TooLong
+				"couldn't parse lexicon ref : : ParseLexRef: failed to split full lexicon name into two colon separated parts: 'lexiconName'"
+			);
+		$status = $this->speechoidConnector->deleteLexiconEntry(
+			'lexiconName',
+			'identity'
+		);
+		$this->assertFalse( $status->isOK() );
+	}
+
+	public function testDeleteLexiconEntry_nonExistingLexiconNameGiven_isNotOK() {
+		$this->config->set( 'WikispeechSpeechoidUrl', 'speechoid.url' );
+		$this->requestFactory
+			->method( 'get' )
+			->with( 'speechoid.url/lexserver/lexicon/delete_entry/lexiconName/identity' )
+			->willReturn(
+			// phpcs:ignore Generic.Files.LineLength.TooLong
+				"failed to detele entry id '1' in lexicon 'lexiconName' : DBManager.DeleteEntry: no such db 'lexiconName'"
+			);
+		$status = $this->speechoidConnector->deleteLexiconEntry(
+			'lexiconName',
+			'identity'
+		);
+		$this->assertFalse( $status->isOK() );
+	}
+
+	public function testDeleteLexiconEntry_nonExistingIdentityGiven_isNotOK() {
+		$this->config->set( 'WikispeechSpeechoidUrl', 'speechoid.url' );
+		$this->requestFactory
+			->method( 'get' )
+			->with( 'speechoid.url/lexserver/lexicon/delete_entry/lexiconName/identity' )
+			->willReturn(
+			// phpcs:ignore Generic.Files.LineLength.TooLong
+				"failed to detele entry id 'identity' in lexicon 'lexiconName' : dbapi.deleteEntry failed to delete entry with id 'identity' from lexicon 'lexiconName'"
+			);
+		$status = $this->speechoidConnector->deleteLexiconEntry(
+			'lexiconName',
+			'identity'
+		);
+		$this->assertFalse( $status->isOK() );
 	}
 }

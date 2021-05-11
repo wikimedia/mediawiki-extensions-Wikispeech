4f27fc66fbaf1fbad88fad5a9d7491163212c577
kalle <karl.wettin@wikimedia.se>
2021-05-11 13:10:07 +0200

    Refactor UtteranceStoreTest
    Extracted seperate unit tests and integration tests.
    
    Change-Id: I256e75ec680d20d4c6125736c1a036861a51d370

---
+++ tests/phpunit/unit/Utterance/UtteranceStoreUrlPathFactoryTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace MediaWiki\Wikispeech\Tests\Unit\Utterance;
+
+use MediaWiki\Wikispeech\Utterance\UtteranceStore;
+use MediaWikiUnitTestCase;
+use Wikimedia\TestingAccessWrapper;
+
+/**
+ * @file
+ * @ingroup Extensions
+ * @license GPL-2.0-or-later
+ */
+
+/**
+ * Asserts functionally of paths in file backend,
+ * which avoids overloading directories with files.
+ *
+ * @covers \MediaWiki\Wikispeech\Utterance\UtteranceStore
+ */
+class UtteranceStoreUrlPathFactoryTest extends MediaWikiUnitTestCase {
+
+	/** @var TestingAccessWrapper|UtteranceStore */
+	private $utteranceStore;
+
+	protected function setUp() : void {
+		parent::setUp();
+		$this->utteranceStore = TestingAccessWrapper::newFromObject( new class extends UtteranceStore {
+			public function __construct() {
+				// override constructor, we don't want to validate configuration for this test.
+			}
+		} );
+	}
+
+	public function dataProvider(): array {
+		return [
+			'1 character integer' => [ '/', 1 ],
+			'2 character integer' => [ '/', 12 ],
+			'3 character integer' => [ '/', 123 ],
+			'4 character integer' => [ '/1/', 1234 ],
+			'5 character integer' => [ '/1/2/', 12345 ],
+			'6 character integer' => [ '/1/2/3/', 123456 ],
+			'7 character integer' => [ '/1/2/3/4/', 1234567 ],
+		];
+	}
+
+	/**
+	 * @dataProvider dataProvider
+	 * @param string $path
+	 * @param int $utteranceId
+	 */
+	public function testUrlPathFactory( string $path, int $utteranceId ) {
+		$this->assertEquals(
+			$path,
+			$this->utteranceStore->urlPathFactory( $utteranceId )
+		);
+	}
+
+}

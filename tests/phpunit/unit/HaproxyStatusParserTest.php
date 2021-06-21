e574519843ab90756dd9c3a6a8624c31aa182104
Sebastian Berlin <sebastian.berlin@wikimedia.se>
2021-06-21 15:32:59 +0200

    Change name of test test_parseCsv_fetchesRowAndColumn to follow convention
    Code sniffer complained about this locally. Not sure why Jenkins does
    not.
    
    Change-Id: Id929a354d056406eefe2b84e7d5399bd0b98ac75

--- tests/phpunit/unit/HaproxyStatusParserTest.php
+++ tests/phpunit/unit/HaproxyStatusParserTest.php
@@ -16,7 +16,7 @@ use MediaWikiUnitTestCase;
  */
 class HaproxyStatusParserTest extends MediaWikiUnitTestCase {
 
-	public function test_parseCsv_fetchesRowAndColumn() {
+	public function testParseCsv_fetchesRowAndColumn() {
 		// phpcs:disable
 		$input = "# pxname,svname,qcur,qmax,scur,smax,slim,stot,bin,bout,dreq,dresp,ereq,econ,eresp,wretr,wredis,status,weight,act,bck,chkfail,chkdown,lastchg,downtime,qlimit,pid,iid,sid,throttle,lbtot,tracked,type,rate,rate_lim,rate_max,check_status,check_code,check_duration,hrsp_1xx,hrsp_2xx,hrsp_3xx,hrsp_4xx,hrsp_5xx,hrsp_other,hanafail,req_rate,req_rate_max,req_tot,cli_abrt,srv_abrt,comp_in,comp_out,comp_byp,comp_rsp,lastsess,last_chk,last_agt,qtime,ctime,rtime,ttime,agent_status,agent_code,agent_duration,check_desc,agent_desc,check_rise,check_fall,check_health,agent_rise,agent_fall,agent_health,addr,cookie,mode,algo,conn_rate,conn_rate_max,conn_tot,intercepted,dcon,dses,
 frontend_1,FRONTEND,,,0,6,2000,177,112516,3918123,0,0,0,,,,,OPEN,,,,,,,,,1,1,0,,,,0,6,0,21,,,,,,,,,,,0,0,0,,,0,0,0,0,,,,,,,,,,,,,,,,,,,,,tcp,,6,21,177,,0,0,

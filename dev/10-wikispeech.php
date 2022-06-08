24b89fc46d71dbb3ace334ced56d6b40d8e4b068
Sebastian Berlin <sebastian.berlin@wikimedia.se>
2022-06-08 15:41:53 +0200

    Add developer config variables for Haproxy and symbolset
    Change-Id: I54a9d0ea1ec14c2bbbdbd8e1adb927eb258e711e

--- dev/10-wikispeech.php
+++ dev/10-wikispeech.php
@@ -6,6 +6,10 @@ $wgGroupPermissions['user']['pagelang'] = true;
 
 // Connect a Speechoid instance
 $wgWikispeechSpeechoidUrl = 'https://wikispeech-tts-dev.wmflabs.org/';
+$wgWikispeechSpeechoidHaproxyQueueUrl = 'https://wikispeech-tts-dev.wmflabs.org/';
+
+// Used to map between symbolsets
+$wgWikispeechSymbolSetUrl = 'https://wikispeech-symbolset-dev.wmcloud.org/';
 
 // Only allow logged in users to listen, to check permissions are respected
 $wgGroupPermissions['*']['wikispeech-listen'] = false;

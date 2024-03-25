309c05c01694fda68668f35c1db51dcea5564414
Sebastian Berlin <sebastian.berlin@wikimedia.se>
2024-03-25 14:47:49 +0100

    New version of Speechoid, 0.1.3
    Adds Sox proxy component that previously had to be downloaded manually.
    
    Updates the content of /dev/speechoid-docker-compose/ to replace external repo
    https://github.com/Wikimedia-Sverige/wikispeech-speechoid-docker-compose/.
    
    There had been some changes to the Blubber pipeline since the last version.
    Speechoid files have been updated to work with these and updates to the
    individual components related to them.
    
    Bug: T351223
    Change-Id: I8e0c658278f390fa1842e6ee18dfd7bb32cbbb9a

--- dev/speechoid-docker-compose/compose-files/mary-entrypoint.sh
+++ dev/speechoid-docker-compose/compose-files/mary-entrypoint.sh
@@ -1,18 +1,40 @@
 #!/usr/bin/env bash
 
 echo "Waiting for all other services to start..."
-
 /srv/compose/wait-for-it.sh -h mishkal -p 8080 -t 60
 
+echo "Configuring HAProxy"
+cat > /srv/haproxy-generated.cfg <<EOF
+global
+	daemon
+	maxconn ${HAPROXY_QUEUE_SIZE:-100}
+
+defaults
+	mode tcp
+	timeout connect ${HAPROXY_TIMEOUT_CONNECT:-60s}
+	timeout client ${HAPROXY_TIMEOUT_CLIENT:-60s}
+	timeout server ${HAPROXY_TIMEOUT_SERVER:-60s}
+
+frontend frontend_1
+	bind *:${HAPROXY_MARY_TTS_FRONTEND_PORT:-8080}
+	default_backend backend_1
+
+backend backend_1
+	server server_1 127.0.0.1:${HAPROXY_MARY_TTS_BACKEND_PORT:-59125} maxconn ${HAPROXY_MARY_TTS_BACKEND_MAXIMUM_CONCURRENT_CONNECTIONS:-1}
+
+frontend stats
+	mode http
+	bind *:8404
+	stats enable
+	stats uri /stats
+	stats refresh ${HAPROXY_STATS_FRONTEND_REFRESH_RATE:-4s}
+	stats admin if TRUE
+EOF
 
 echo "Starting HAProxy"
-/usr/sbin/haproxy -f /srv/haproxy.cfg
+/usr/sbin/haproxy -f /srv/haproxy-generated.cfg
 
-echo "Starting Mary TTS STTS."
+echo "Starting Mary TTS"
 export GRADLE_USER_HOME=/srv/gradle_user_home
-#export MARY_TTS_MISHKAL_URL=http://localhost:8080/
-#cd src/marytts
-#./gradlew run --no-rebuild -Dmodules.poweronselftest=false
 cd /srv/mary-tts/build/install/mary-tts/
 ./bin/marytts-server
-#/bin/bash
--- dev/speechoid-docker-compose/compose-files/pronlex-entrypoint.sh
+++ dev/speechoid-docker-compose/compose-files/pronlex-entrypoint.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/env bash
 
-echo "Waiting for all other services to start..."
-
-/srv/compose/wait-for-it.sh -h mariadb -p 3306 -t 120
-
 echo "Starting Pronlex..."
 
 DIR=`pwd`
 
-export GOROOT=${DIR}/go
-export GOPATH=${DIR}/goProjects
-export PATH=${GOPATH}/bin:${GOROOT}/bin:${PATH}
-
 cd pronlex
 
-/bin/bash scripts/start_server.sh -a ${DIR}/appdir -e sqlite -p 8787
+if [[ -z "${PRONLEX_MARIADB_URI}" ]]; then
+  /bin/bash scripts/start_server.sh -a ${DIR}/appdir -e sqlite -p 8787 -r lexserver
+else
+  echo "Waiting for all other services to start..."
+  /srv/compose/wait-for-it.sh -h mariadb -p 3306 -t 120
+
+  /bin/bash scripts/start_server.sh -a ${DIR}/appdir -e mariadb -l "${PRONLEX_MARIADB_URI}" -p 8787 -r lexserver
+fi

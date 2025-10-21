ef7065e5fd6469d582d6dcdeaf7dd11f6d5e8d99
Viktoria Hillerud WMSE <viktoria.hillerud@wikimedia.se>
2025-10-21 10:34:57 +0200

    Previewing doesn't work on Special:EditLexicon
    Bug: T406559
    Change-Id: I127608a6de0bdb85b990e8270bf931f7e95e707b

--- modules/ext.wikispeech.specialEditLexicon.js
+++ modules/ext.wikispeech.specialEditLexicon.js
@@ -1,30 +1,31 @@
 const Previewer = require( './ext.wikispeech.transcriptionPreviewer.js' );
+
+mw.loader.using( 'mediawiki.api' ).then( () => {
 // eslint-disable-next-line no-jquery/no-global-selector
-const $content = $( '#mw-content-text' );
-const $language = $content.find( '#ext-wikispeech-language' ).find( 'select, input' );
-const $transcription = $content.find( '#ext-wikispeech-transcription input' );
-const api = new mw.Api();
-const $previewPlayer = $( '<audio>' ).insertAfter( $transcription );
-const previewer = new Previewer( $language, $transcription, api, $previewPlayer );
-
-// Toggles raw JSON view
-const $rawJson = $content.find( '.toggle-raw' );
-$rawJson.on( 'click', function () {
-	const targetId = this.getAttribute( 'data-target' );
-	const $target = $( '#' + targetId );
-	$target.toggle();
-} );
-
-const previewButton = OO.ui.infuse( $content.find( '#ext-wikispeech-preview-button' ) );
-previewButton.on(
-	'click',
-	() => {
-		previewButton.setDisabled( true );
-		previewer.play().then( () => {
-			previewButton.setDisabled( false );
-		},
+	const $content = $( '#mw-content-text' );
+	const $language = $content.find( '#ext-wikispeech-language' ).find( 'select, input' );
+	const $transcription = $content.find( '#ext-wikispeech-transcription input' );
+	const api = new mw.Api();
+	const $previewPlayer = $( '<audio>' ).insertAfter( $transcription );
+	const previewer = new Previewer( $language, $transcription, api, $previewPlayer );
+	// Toggles raw JSON view
+	const $rawJson = $content.find( '.toggle-raw' );
+	$rawJson.on( 'click', function () {
+		const targetId = this.getAttribute( 'data-target' );
+		const $target = $( '#' + targetId );
+		$target.toggle();
+	} );
+	const previewButton = OO.ui.infuse( $content.find( '#ext-wikispeech-preview-button' ) );
+	previewButton.on(
+		'click',
 		() => {
-			previewButton.setDisabled( false );
-		} );
-	}
-);
+			previewButton.setDisabled( true );
+			previewer.play().then( () => {
+				previewButton.setDisabled( false );
+			},
+			() => {
+				previewButton.setDisabled( false );
+			} );
+		}
+	);
+} );
